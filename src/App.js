e35d258a012f343e6b969a44fc2799f8607a3d79
BillelBenoudjit <hb_benoudjit@esi.dz>
2021-11-10 21:01:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Container, Switch, withStyles } from '@material-ui/core'
+import Header from './components/Header/Header';
+import { grey } from '@material-ui/core/colors';
+
 
 function App() {
+  const [text, setText] = useState("")
+  const [language, setLanguage] = useState("fr-FR")
+  const [lightMode, setLightMode] = useState(false)
+
+  const ThemeSwitch = withStyles({
+    switchBase: {
+      color: grey[300],
+      '&$checked': {
+        color: grey[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: lightMode ? "#fff" : "#282c34",
+        color: lightMode ? "#000" : "#fff",
+        transition: "all 0.5s linear"
+      }}
+    >
+      <Container
+        maxWidth='md'
+        style={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "space-evenly" }}
+      >
+        <div style={{ position: "absolute", top: "0", right: "15px", paddingTop: "10" }}>
+          <span>{lightMode ? "Dark" : "Light"} Mode</span>
+          <ThemeSwitch checked={lightMode} onChange={() => setLightMode(!lightMode)} />
+        </div>
+        <Header
+          language={language} setLanguage={setLanguage}
+          text={text} setText={setText}
+          lightMode={lightMode}
+        />
+      </Container>
+    </div >
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,53 @@
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import React from 'react'
+import MicIcon from '@material-ui/icons/Mic';
+import { SpeechConfig, AudioConfig, SpeechRecognizer, ResultReason } from 'microsoft-cognitiveservices-speech-sdk'
+import './Header.css'
+
+const Header = ({ language, setLanguage, text, setText, lightMode }) => {
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: lightMode ? '#000' : '#fff'
+            },
+            type: lightMode ? 'light' : 'dark',
+        },
+    });
+
+    const sttFromMic = async () => {
+        const speechConfig = SpeechConfig.fromSubscription(process.env.REACT_APP_SPEECH_KEY, process.env.REACT_APP_SPEECH_REGION)
+        speechConfig.speechRecognitionLanguage = language
+
+        const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+        const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        alert(language === 'en' ? 'Speak into your microphone in English...' : 'Parlez dans votre microphone en Français...')
+
+        recognizer.recognizeOnceAsync(result => {
+            let text;
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                text = result.text.replace('.', "")
+            } else {
+                alert('ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.')
+                text = '';
+            }
+            setText(text)
+        });
+    }
+
+    return (
+        <div className="header">
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <MicIcon className="mic" onClick={() => { sttFromMic() }} />
+                    <h1 className="select">
+                        Parler pour obtenir votre texte.
+                    </h1>
+                </ThemeProvider>
+            </div>
+            <p className="title">{text ? text : ""}</p>
+        </div>
+    )
+}
+
+export default Header
