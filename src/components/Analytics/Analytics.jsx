36654ff56bdb4a7e5a066aa84db3a8874a43ede0
BillelBenoudjit <hb_benoudjit@esi.dz>
2021-11-29 20:10:31 +0100

    Add Key phrases extraction

--- src/components/Analytics/Analytics.jsx
+++ src/components/Analytics/Analytics.jsx
@@ -20,6 +20,8 @@ const Analytics = ({ text, lightMode }) => {
     const [textAnalytics, setTextAnalytics] = useState([])
     const [translatedText, setTranslatedText] = useState("")
     const [textEntities, setTextEntities] = useState([])
+    // const [results, setResults] = useState([])
+    const [data, setData] = useState([])
 
     const textAnalyticsClient = new TextAnalyticsClient(
         process.env.REACT_APP_LANGUAGE_ENDPOINT,
@@ -69,12 +71,16 @@ const Analytics = ({ text, lightMode }) => {
         }
     }
 
-    const summarization_example = async (client) => {
-        const documents = [`The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. 
-            These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. 
-            They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. 
-            In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. 
-            It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.`];
+    const summarization_example = async (client, text) => {
+        // let documents = [`La fonction de résumé extractif utilise des techniques de traitement du langage naturel pour localiser les phrases clés dans un document textuel non structuré. 
+        // Ces phrases transmettent collectivement l'idée principale du document. Cette fonction est fournie sous forme d'API aux développeurs. 
+        // Ils peuvent l'utiliser pour élaborer des solutions intelligentes basées sur les informations pertinentes extraites afin de prendre en charge divers cas d'utilisation. 
+        // Dans l'aperçu public, le résumé extractif prend en charge plusieurs langues. Il est basé sur des modèles de transformation multilingues pré-entraînés, dans le cadre de notre quête de représentations holistiques. 
+        // Il tire sa force de l'apprentissage par transfert à travers les langues monolingues et exploite la nature partagée des langues pour produire des modèles de qualité et d'efficacité améliorées.`];
+
+        let documents = []
+
+        documents.push(text)
 
         console.log("== Analyze Sample For Extract Summary ==");
 
@@ -86,9 +92,10 @@ const Analytics = ({ text, lightMode }) => {
         const actions = {
             recognizeEntitiesActions: [{ modelVersion: "latest" }],
             recognizePiiEntitiesActions: [{ modelVersion: "latest" }],
-            extractKeyPhrasesActions: [{ modelVersion: "latest" }]
+            extractKeyPhrasesActions: [{ modelVersion: "latest" }],
+            ExtractSummaryActions: [{ modelVersion: "latest" }]
         };
-        const poller = await client.beginAnalyzeActions(documents, actions, "en", {
+        const poller = await client.beginAnalyzeActions(documents, actions, "fr", {
             includeStatistics: true
         })
 
@@ -106,59 +113,76 @@ const Analytics = ({ text, lightMode }) => {
 
         const resultPages = await poller.pollUntilDone();
 
-        console.log(resultPages)
-
         for await (const page of resultPages) {
             const keyPhrasesAction = page.extractKeyPhrasesResults[0];
-            if (!keyPhrasesAction.error) {
-                for (const doc of keyPhrasesAction.results) {
-                    console.log(`- Document ${doc.id}`);
-                    if (!doc.error) {
-                        console.log("\tKey phrases:");
-                        for (const phrase of doc.keyPhrases) {
-                            console.log(`\t- ${phrase}`);
-                        }
-                    } else {
-                        console.error("\tError:", doc.error);
-                    }
-                }
-                console.log("Action statistics: ");
-                console.log(JSON.stringify(keyPhrasesAction.results.statistics));
-            }
 
-            const entitiesAction = page.recognizeEntitiesResults[0];
-            if (!entitiesAction.error) {
-                for (const doc of entitiesAction.results) {
-                    console.log(`- Document ${doc.id}`);
-                    if (!doc.error) {
-                        console.log("\tEntities:");
-                        for (const entity of doc.entities) {
-                            console.log(`\t- Entity ${entity.text} of type ${entity.category}`);
-                        }
-                    } else {
-                        console.error("\tError:", doc.error);
-                    }
-                }
-                console.log("Action statistics: ");
-                console.log(JSON.stringify(entitiesAction.results.statistics));
-            }
+            console.log(keyPhrasesAction.results)
+            //await setResults(keyPhrasesAction.results)
 
-            const piiEntitiesAction = page.recognizePiiEntitiesResults[0];
-            if (!piiEntitiesAction.error) {
-                for (const doc of piiEntitiesAction.results) {
-                    console.log(`- Document ${doc.id}`);
-                    if (!doc.error) {
-                        console.log("\tPii Entities:");
-                        for (const entity of doc.entities) {
-                            console.log(`\t- Entity ${entity.text} of type ${entity.category}`);
-                        }
-                    } else {
-                        console.error("\tError:", doc.error);
-                    }
-                }
-                console.log("Action statistics: ");
-                console.log(JSON.stringify(piiEntitiesAction.results.statistics));
-            }
+            return keyPhrasesAction.results
+
+            // keyPhrasesAction.results?.map((doc) => {
+            //     console.log(`- Document ${doc.id}`)
+            //     console.log("\tKey phrases:")
+            //     doc.keyPhrases.map((phrase) => {
+            //         console.log(`\t- ${phrase}`);
+            //     })
+            // })
+
+            // if (!keyPhrasesAction.error) {
+            //     for (const doc of keyPhrasesAction.results) {
+            //         console.log(`- Document ${doc.id}`);
+            //         if (!doc.error) {
+            //             console.log("\tKey phrases:");
+            //             for (const phrase of doc.keyPhrases) {
+            //                 console.log(`\t- ${phrase}`);
+            //             }
+            //         } else {
+            //             console.error("\tError:", doc.error);
+            //         }
+            //     }
+            //     console.log("Action statistics: ");
+            //     console.log(JSON.stringify(keyPhrasesAction.results.statistics));
+
+            // }
+
+            // const extractSummaryActionResult = page.extractSummaryActionResults[0]
+
+            // console.log(extractSummaryActionResult)
+
+            // const entitiesAction = page.recognizeEntitiesResults[0];
+            // if (!entitiesAction.error) {
+            //     for (const doc of entitiesAction.results) {
+            //         console.log(`- Document ${doc.id}`);
+            //         if (!doc.error) {
+            //             console.log("\tEntities:");
+            //             for (const entity of doc.entities) {
+            //                 console.log(`\t- Entity ${entity.text} of type ${entity.category}`);
+            //             }
+            //         } else {
+            //             console.error("\tError:", doc.error);
+            //         }
+            //     }
+            //     console.log("Action statistics: ");
+            //     console.log(JSON.stringify(entitiesAction.results.statistics));
+            // }
+
+            // const piiEntitiesAction = page.recognizePiiEntitiesResults[0];
+            // if (!piiEntitiesAction.error) {
+            //     for (const doc of piiEntitiesAction.results) {
+            //         console.log(`- Document ${doc.id}`);
+            //         if (!doc.error) {
+            //             console.log("\tPii Entities:");
+            //             for (const entity of doc.entities) {
+            //                 console.log(`\t- Entity ${entity.text} of type ${entity.category}`);
+            //             }
+            //         } else {
+            //             console.error("\tError:", doc.error);
+            //         }
+            //     }
+            //     console.log("Action statistics: ");
+            //     console.log(JSON.stringify(piiEntitiesAction.results.statistics));
+            // }
         }
     }
 
@@ -213,12 +237,23 @@ const Analytics = ({ text, lightMode }) => {
         }
     }
 
+    // healthExample(textAnalyticsClient).catch((err) => {
+    //     console.error("The sample encountered an error:", err)
+    // })
+
     // summarization_example(textAnalyticsClient).catch((err) => {
     //     console.error("The sample encountered an error:", err);
     // });
 
-    useEffect(() => {
-        entityRecognition(textAnalyticsClient, text)
+    useEffect(async () => {
+        await entityRecognition(textAnalyticsClient, text)
+        if (text) {
+            let results = await summarization_example(textAnalyticsClient, text)
+            console.log(results)
+
+            let phrases = results[0].keyPhrases.map((phrase) => phrase)
+            setData(phrases)
+        }
     }, [text])
 
     return (
@@ -240,43 +275,75 @@ const Analytics = ({ text, lightMode }) => {
                         : "De la parole au texte!"}</b>
                     <hr style={{ backgroundColor: "black", width: "100%" }} />
                 </div>
-                {text ?
-                    <TableContainer
-                        component={Paper}
-                        style={{
-                            borderRadius: "10px"
-                        }}
-                    >
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>ID</TableCell>
-                                    <TableCell align="right">Texte</TableCell>
-                                    <TableCell align="right">Categorie</TableCell>
-                                    <TableCell align="right">Sous catégorie</TableCell>
-                                    <TableCell align="right">Score de confiance</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {
-                                    textAnalytics[0]?.entities.map((entity, id) => (
-                                        <TableRow
-                                            key={id}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row">
-                                                {textAnalytics[0].id}
-                                            </TableCell>
-                                            <TableCell align="right">{entity.text}</TableCell>
-                                            <TableCell align="right">{entity.category}</TableCell>
-                                            <TableCell align="right">{entity.subCategory ? entity.subCategory : "N/A"}</TableCell>
-                                            <TableCell align="right">{entity.confidenceScore}</TableCell>
+                <div>
+                    {text ?
+                        <>
+                            <h3 className="select">
+                                Named Entity Recognition
+                </h3>
+                            <TableContainer
+                                component={Paper}
+                                style={{
+                                    borderRadius: "10px"
+                                }}
+                            >
+                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>ID</TableCell>
+                                            <TableCell align="right">Texte</TableCell>
+                                            <TableCell align="right">Catégorie</TableCell>
+                                            <TableCell align="right">Sous catégorie</TableCell>
+                                            <TableCell align="right">Score de confiance</TableCell>
                                         </TableRow>
-                                    ))
-                                }
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                                    </TableHead>
+                                    <TableBody>
+                                        {
+                                            textAnalytics[0]?.entities.map((entity, id) => (
+                                                <TableRow
+                                                    key={id}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">
+                                                        {textAnalytics[0].id}
+                                                    </TableCell>
+                                                    <TableCell align="right">{entity.text}</TableCell>
+                                                    <TableCell align="right">{entity.category}</TableCell>
+                                                    <TableCell align="right">{entity.subCategory ? entity.subCategory : "N/A"}</TableCell>
+                                                    <TableCell align="right">{entity.confidenceScore}</TableCell>
+                                                </TableRow>
+                                            ))
+                                        }
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </>
+                        : <br></br>
+                    }
+                </div>
+                {text ?
+                    <>
+                        <h3 className="select">
+                            Key phrases Extraction
+                    </h3>
+                        <div
+                            className="singleMean"
+                            style={{
+                                backgroundColor: lightMode ? "#3B5360" : "#fff",
+                                color: lightMode ? "#fff" : "#000",
+                            }}
+                        >
+                            {
+                                data.map((word) => {
+                                    console.log(word)
+                                    return (
+                                        <li styles={{ fontWeight: 'bold' }}>{word}</li>
+                                    )
+                                })
+                            }
+                            <hr style={{ backgroundColor: "black", width: "100%" }} />
+                        </div>
+                    </>
                     : <br></br>
                 }
             </div >
